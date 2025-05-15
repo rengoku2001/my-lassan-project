6674458fd482df6fdb32e1b87f9ba68e3241ca75
venkatesh <venkateshjatavath2@gmail.com>
2025-05-15 23:22:26 +0530

    changes

--- vpc-network/provider.tf
+++ vpc-network/provider.tf
@@ -2,3 +2,15 @@ provider "aws" {
     region = "ap-south-1"
   
 }
+
+
+terraform {
+  backend "s3" {
+    bucket = "duckbhai"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+    encrypt = true
+    use_lockfile = true
+    
+  }
+}
